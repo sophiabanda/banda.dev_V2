2050c992b2d5f0fda4c8c7d28fe8d843bc77cba5
Sophia Banda <sophia.m.banda@gmail.com>
2026-09-13 09:02:23 -0400

    removed unused scripts

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const focusArea = document.getElementsByTagName('div')
     const h1Text = `Hi. I'm<br> Sophia Banda.`
     let i = 0
 
@@ -19,36 +18,4 @@ document.addEventListener('DOMContentLoaded', function () {
             this.setTimeout(typeEffect, 100)
         }
     })
-
-    focusArea.addEventListener('click', (e) => {
-        focusArea.classList.add('focus-section')
-    })
 })
-
-window.addEventListener(
-    'mousemove',
-    function (e) {
-        const arr = [1, 0.9, 0.8, 0.5, 0.2]
-
-        arr.forEach(function (i) {
-            var x = (1 - i) * 75
-            var star = document.createElement('div')
-
-            star.className = 'star'
-            star.style.top =
-                e.pageY + Math.round(Math.random() * x - x / 2) + 'px'
-            star.style.left =
-                e.pageX + Math.round(Math.random() * x - x / 2) + 'px'
-
-            document.body.appendChild(star)
-
-            window.setTimeout(
-                function () {
-                    document.body.removeChild(star)
-                },
-                Math.round(Math.random() * i * 600),
-            )
-        })
-    },
-    false,
-)
