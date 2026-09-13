9902f1df99e0dc09029aec75275d13a9778dfb1b
Sophia Banda <sophia.m.banda@gmail.com>
2026-09-13 14:19:24 -0400

    udpates structure for mobile, styling, and larger screen size

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const h1Text = `Hi. I'm<br> Sophia Banda.`
-    let i = 0
+const items = document.querySelectorAll('.resume-item')
+const h1 = document.querySelector('h1')
 
-    window.addEventListener('load', function typeEffect() {
-        if (i < h1Text.length) {
-            const currentChar = h1Text.charAt(i)
-            if (currentChar === '<') {
-                const brTag = h1Text.substring(i, i + 4)
-                if (brTag === '<br>') {
-                    this.document.querySelector('h1').innerHTML += '<br>'
-                    i += 4
-                }
-            } else {
-                this.document.querySelector('h1').innerHTML += currentChar
-                i++
-            }
-            this.setTimeout(typeEffect, 100)
-        }
-    })
-})
+const h1Text = `Hi. I'm<br> Sophia Banda.`
+let i = 0
+
+function typeEffect() {
+    if (i >= h1Text.length) return
+
+    const currentChar = h1Text.charAt(i)
+
+    if (h1Text.substring(i, i + 4) === '<br>') {
+        h1.innerHTML += '<br>'
+        i += 4
+    } else {
+        h1.innerHTML += currentChar
+        i++
+    }
+
+    setTimeout(typeEffect, 100)
+}
+
+typeEffect()
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            entry.target.classList.toggle('in-focus', entry.isIntersecting)
+        })
+    },
+    {
+        threshold: 0.6,
+    },
+)
+
+items.forEach((item) => observer.observe(item))
